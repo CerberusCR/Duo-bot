f50d52d506ae0fbf9c6fdc86b5630c6af1f62d33
taigo <taigo@cock.li>
2025-01-13 22:45:06 +0100

    some structure

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,36 @@ import { Client, GatewayIntentBits } from "discord.js";
 import env from "../environment.json";
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds, // For guild-related events
+    GatewayIntentBits.GuildMessages, // To receive messages in guilds
+    GatewayIntentBits.MessageContent, // To access message content
+  ],
+});
 
 // When the bot is ready
 client.once("ready", () => {
   console.log("Bot is online!");
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  console.log(message);
+  if (message.content === "!guh") {
+    if (!message.guild) return;
+    const botMember = message.guild.members.me;
+    if (botMember?.permissions.has("ManageMessages")) {
+      await message.delete();
+
+      try {
+        const user = await client.users.fetch("");
+        await user.send("");
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
 });
 
 // Log in to Discord with your bot token
