e933cfd51ced1b7ae07e22eded5c63aff62a1c72
taigo <taigo@cock.li>
2025-01-19 15:59:24 +0100

    greatly improve code quality

--- src/extras/checkStreak.ts
+++ src/extras/checkStreak.ts
@@ -2,15 +2,29 @@ import { User } from "discord.js";
 import luoDingoUsers from "../../connected-users.json";
 import { fetchUrl } from "../api";
 
+interface connectedUser {
+  discord_id: string;
+  discord_username: string;
+  duolingo_username: string;
+}
+
 export async function checkStreak(discordUser: User | null) {
-  console.log(luoDingoUsers);
-  const conUser = luoDingoUsers.users.find(
-    (conUser) => conUser.discord_username === discordUser?.username,
+  // console.log(luoDingoUsers);
+  const connectedUser: connectedUser | undefined = luoDingoUsers.users.find(
+    (connectedUser: connectedUser) =>
+      connectedUser?.discord_username === discordUser?.username,
   );
-  const userData: ApiResponse | undefined = await fetchUrl(
-    `https://www.duolingo.com/2017-06-30/users?username=${conUser?.duolingo_username}`,
-  );
-  const luoUser = userData?.users[0];
+  if (!connectedUser) return;
+  const { duolingo_username } = connectedUser;
+  let duolingoUserData: ApiResponse | undefined;
+  try {
+    duolingoUserData = await fetchUrl(
+      `https://www.duolingo.com/2017-06-30/users?username=${duolingo_username}`,
+    );
+  } catch (e) {
+    console.error(e);
+  }
+  const luoUser = duolingoUserData?.users[0];
   if (luoUser?.streak == 0) {
     discordUser?.send(
       "Your streak is crying in the corner, do you want to join it?",
